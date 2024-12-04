7096131bc3354a9a15f00999ba5f331acbb3b0a3
dxquyet16 <quyetcoder16@gmail.com>
2024-12-04 22:09:37 +0700

    init

---
+++ osahan_eat/src/main/java/com/quyet/osahan_eat/OsahanEatApplication.java
@@ -0,0 +1,13 @@
+package com.quyet.osahan_eat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsahanEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsahanEatApplication.class, args);
+	}
+
+}
---
+++ osahan_eat/src/test/java/com/quyet/osahan_eat/OsahanEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quyet.osahan_eat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsahanEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
