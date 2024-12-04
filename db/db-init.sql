da2dd64222c6c4f7655cc9cd77e135fe59b5e7b7
dxquyet16 <quyetcoder16@gmail.com>
2024-12-04 19:39:15 +0700

    first commit

---
+++ db/db-init.sql
@@ -0,0 +1,121 @@
+drop database if exists osahaneat;
+create database osahaneat;
+
+use osahaneat;
+
+create table Roles(
+	id int auto_increment,
+    role_name varchar(20),
+    create_date timestamp,
+    primary key(id)
+);
+
+create table Users(
+	id int auto_increment,
+    user_name varchar(100),
+    password varchar(100),
+    full_name varchar(50),
+    create_date timestamp,
+    role_id int,
+    primary key(id)
+);
+
+create table RatingFood(
+	id int auto_increment,
+    user_id int,
+    food_id int,
+    content text,
+    rate_point int(5),
+    
+    primary key(id)
+);
+
+create table Category(
+	id int auto_increment,
+    name_cate varchar(100),
+    create_date timestamp,
+    primary key(id)
+);
+
+create table Food(
+	id int auto_increment,
+    title varchar(255),
+    image text,
+    time_ship varchar(10),
+    price decimal,
+    cate_id int,
+    primary key(id)
+);
+
+create table RatingRestaurant(
+	id int auto_increment,
+    user_id int,
+    res_id int,
+    content text,
+    rate_point int(5),
+    
+    primary key(id)
+);
+
+
+create table Orders(
+	id int auto_increment,
+    user_id int,
+    res_id int,
+    create_date timestamp,
+    primary key(id)
+);
+
+create table MenuRestaurant(
+	cate_id int,
+    res_id int,
+    create_date timestamp,
+    
+    primary key(res_id,cate_id)
+);
+
+create table Restaurant(
+	id int auto_increment,
+    title varchar(255),
+    subtitle varchar(255),
+    description text,
+    image text,
+    is_freeship boolean,
+    address varchar(255),
+    open_date timestamp,
+    
+    primary key(id)
+);
+
+create table Promo(
+	id int auto_increment,
+    res_id int,
+    percent int,
+    start_date timestamp,
+    end_date timestamp,
+    primary key(id)
+);
+
+create table OrderItem(
+	order_id  int,
+    food_id int,
+    quantity int,
+    create_date timestamp,
+    
+    primary key(order_id,food_id)
+);
+
+
+alter table Users add constraint FK_USERS_ROLE_ID foreign key(role_id) references Roles(id);
+alter table RatingFood add constraint FK_RATINGFOOD_USER_ID foreign key(user_id) references Users(id);
+alter table RatingFood add constraint FK_RATINGFOOD_food_ID foreign key(food_id) references Food(id);
+alter table Food add constraint fk_food_cate_id foreign key(cate_id) references Category(id);
+alter table RatingRestaurant add constraint fk_ratingrestaurant_user_id foreign key(user_id) references Users(id);
+alter table RatingRestaurant add constraint fk_ratingrestaurant_res_id foreign key(res_id) references Restaurant(id);
+alter table Orders add constraint fk_orders_user_id foreign key(user_id) references Users(id);
+alter table Orders add constraint fk_orders_res_id foreign key(res_id) references Restaurant(id);
+alter table OrderItem add constraint fk_orderitem_order_id foreign key(order_id) references Orders(id);
+alter table OrderItem add constraint fk_orderitem_food_id foreign key(food_id) references Food(id);
+alter table MenuRestaurant add constraint fk_menurestaurant_cate_id foreign key(cate_id) references Category(id);
+alter table MenuRestaurant add constraint fk_menurestaurant_res_id foreign key(res_id) references Restaurant(id);
+alter table Promo add constraint fk_promo_res_id foreign key(res_id) references Restaurant(id);
